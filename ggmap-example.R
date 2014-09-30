7e7058249a9087a9291730434c427f1546b2597f
Manuel Amunategui <amunategui@gmail.com>
2014-09-29 20:07:01 -0700

    Cleaner version

--- ggmap-example.R
+++ ggmap-example.R
@@ -1,8 +1,6 @@
 require(RCurl)
 require(xlsx)
 
-
-# Load data from Hadley Wickham on Github 
 # NOTE if you can't download the file automatically, download it manually at:
 # 'http://www.psc.isr.umich.edu/dis/census/Features/tract2zip/'
 urlfile <-'http://www.psc.isr.umich.edu/dis/census/Features/tract2zip/MedianZIP-3.xlsx'
@@ -10,31 +8,29 @@ destfile <- "census20062010.xlsx"
 download.file(urlfile, destfile, mode="wb")
 census <- read.xlsx2(destfile, sheetName = "Median")
 
-# 
-# setwd('//wn2123/so_company2$/EpicReporting/Reports/WIP/Manuel/ALLDATA')
-# census <- read.csv('MedianZIP-3.csv')
-census <- census[c('Zip','Median')]
-
-# pad each zip codes with zeros in front if missing
-census$Zip <- sprintf("%05d", as.numeric(census$Zip))
+# clean up data
+census <- census[c('Zip','Median..')]
+names(census) <- c('Zip','Median')
 census$Median <- as.character(census$Median)
 census$Median <- as.numeric(gsub(',','',census$Median))
+print(head(census,5))
 
-
-library(zipcode)
+# get geographical coordinates from zipcode
+require(zipcode)
 data(zipcode)
 census$Zip <- clean.zipcodes(census$Zip)
 census <- merge(census, zipcode, by.x='Zip', by.y='zip')
 
-
-library(ggmap)
-library("ggplot2")
-library("RColorBrewer")
-map<-get_map(location='united states', zoom=4, maptype = "roadmap",
+# get a Google map
+require(ggmap)
+map<-get_map(location='united states', zoom=4, maptype = "terrain",
              source='google',color='color')
 
-ggmap(map)+
-        geom_point(
+# plot it with ggplot2
+require("ggplot2")
+require("RColorBrewer")
+ggmap(map) + geom_point(
                 aes(x=longitude, y=latitude, show_guide = TRUE, colour=Median), 
                 data=census, alpha=.8, na.rm = T)  + 
         scale_color_gradient(low="beige", high="blue")
+       
